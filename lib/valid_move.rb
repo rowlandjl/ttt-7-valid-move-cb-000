4ed66b74e645110d35867828b24678df40b4f4ad
Jessica Rowland <rowlandjl@vcu.edu>
2019-09-22 15:37:29 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,5 +4,6 @@ end
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-def position_taken?
-end 
+def position_taken?(board, index)
+  if
+end
