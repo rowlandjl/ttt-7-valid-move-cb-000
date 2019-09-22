777b85fb7d6d1d5aad4a06354e2ef454f29eb8cc
Jessica Rowland <rowlandjl@vcu.edu>
2019-09-22 15:43:54 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if index.between?(0, 8) && !position_taken?(board, index)
-    true
-  end
+  index.between?(0, 8) && !position_taken?(board, index)
 end
 
 
